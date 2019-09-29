e23b85f1ecf8707ecf53c8476ff529075c938352
sigfried <rssanjuan2704@gmail.com>
2019-09-29 18:13:41 -0500

    [MDF] Counter now is a HOC.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,38 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { INCREMENT, DECREMENT } from "../actions";
 
 import "./Counter.css";
 
-const Counter = props => {
-  const counter = useSelector(state => state.counter);
-  const dispatch = useDispatch();
-  return (
-    <div className="counter">
-      <span className="counter__value">{counter}</span>
-      <div className="counter__buttons">
-        <button
-          className="button"
-          onClick={() => dispatch({ type: "INCREMENT" })}
-        >
-          +
-        </button>
-        <button
-          className="button"
-          onClick={() => dispatch({ type: "DECREMENT" })}
-        >
-          -
-        </button>
-      </div>
-    </div>
-  );
-};
+class Counter extends Component {
+  handleIncrement = () => {
+    this.props.dispatch({ type: INCREMENT });
+  };
 
-export default Counter;
+  handleDecrement = () => {
+    this.props.dispatch({ type: DECREMENT });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <span className="counter__value">{this.props.counter}</span>
+        <div className="counter__buttons">
+          <button className="button" onClick={this.handleIncrement}>
+            +
+          </button>
+          <button className="button" onClick={this.handleDecrement}>
+            -
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+export default connect(mapStateToProps)(Counter);
